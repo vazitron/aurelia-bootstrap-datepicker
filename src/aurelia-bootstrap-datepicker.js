257f982e9e21307326d724694dc21164bf0bdf66
Thomas Seberechts <thomas@itprosolutions.be>
2016-05-11 12:07:38 +0200

    changed changeDate event to changedate

--- src/aurelia-bootstrap-datepicker.js
+++ src/aurelia-bootstrap-datepicker.js
@@ -17,7 +17,7 @@ export class AureliaBootstrapDatepicker {
     let self = this;
     $(this.datepicker).datepicker(this.dpOptions)
       .on('changeDate', function(e) {
-        let changeDateEvent = new CustomEvent('changeDate', {detail: {event: e}, bubbles: true});
+        let changeDateEvent = new CustomEvent('changedate', {detail: {event: e}, bubbles: true});
         self.element.dispatchEvent(changeDateEvent);
       });
   }
