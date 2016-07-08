7d4a1ddd7413363b3b8ec029bb66f599b9e1e7c9
Martin Rebane <martin@linuxator.com>
2016-07-08 12:50:01 +0300

    Improve bundling, build

--- dist/amd/index.js
+++ dist/amd/index.js
@@ -1,11 +1,13 @@
-define(['exports'], function (exports) {
+define(['exports', './aurelia-bootstrap-datepicker'], function (exports, _aureliaBootstrapDatepicker) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
+  exports.AureliaBootstrapDatepicker = undefined;
   exports.configure = configure;
   function configure(config) {
     config.globalResources('./aurelia-bootstrap-datepicker');
   }
+  exports.AureliaBootstrapDatepicker = _aureliaBootstrapDatepicker.AureliaBootstrapDatepicker;
 });
--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -3,7 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.AureliaBootstrapDatepicker = undefined;
 exports.configure = configure;
+
+var _aureliaBootstrapDatepicker = require('./aurelia-bootstrap-datepicker');
+
 function configure(config) {
   config.globalResources('./aurelia-bootstrap-datepicker');
 }
+exports.AureliaBootstrapDatepicker = _aureliaBootstrapDatepicker.AureliaBootstrapDatepicker;
