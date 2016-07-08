f89b0f3aa99a5e5afd714ca3fb5b543c4f0edd89
Martin Rebane <martin@linuxator.com>
2016-07-08 12:49:32 +0300

    Improve bundling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import {AureliaBootstrapDatepicker} from './aurelia-bootstrap-datepicker';
 export function configure(config) {
   config.globalResources('./aurelia-bootstrap-datepicker');
 }
+export {AureliaBootstrapDatepicker};
