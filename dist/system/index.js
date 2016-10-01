8e6634c49ee470f3a7996ae6d797b3327ecfca9a
Cyprian Gracz <cyprian.gracz@micro-jumbo.eu>
2016-10-01 23:07:24 +0200

    Updated dependencies

--- dist/system/index.js
+++ dist/system/index.js
@@ -4,17 +4,17 @@ System.register(['./aurelia-bootstrap-datepicker'], function (_export, _context)
   "use strict";
 
   var AureliaBootstrapDatepicker;
+  function configure(config) {
+    config.globalResources('./aurelia-bootstrap-datepicker');
+  }
+
+  _export('configure', configure);
+
   return {
     setters: [function (_aureliaBootstrapDatepicker) {
       AureliaBootstrapDatepicker = _aureliaBootstrapDatepicker.AureliaBootstrapDatepicker;
     }],
     execute: function () {
-      function configure(config) {
-        config.globalResources('./aurelia-bootstrap-datepicker');
-      }
-
-      _export('configure', configure);
-
       _export('AureliaBootstrapDatepicker', AureliaBootstrapDatepicker);
     }
   };
